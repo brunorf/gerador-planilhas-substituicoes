80bcbb1eb796c074ec0a47506c317c156fdd977b
Bruno Rogério Fernandes <brogeriofernandes@gmail.com>
2024-11-01 11:54:02 -0300

    Inclusão de nova class para representar as ocorrências e correção dos calculos de valores de grat.

--- GeradorPlanilhasSubstituicoes.py
+++ GeradorPlanilhasSubstituicoes.py
@@ -45,9 +45,17 @@ class Substituto(Servidor):
 class Titular(Servidor):
     def __init__(self):
         super().__init__()
+        self.substitutos = []
+
+
+class Ocorrencia:
+    def __init__(self, titular):
+        self.titular = titular
         self.motivo_impedimento = None
         self.dias_ocorrencia = 0
-        self.substitutos = []
+        self.mes_ocorrencia = None
+        self.ano_ocorrencia = None
+        self.periodo = None
 
 
 def get_doc():
@@ -146,13 +154,13 @@ def cria_servidor_da_planilha(
     )
 
     servidor.valor_275 = (
-        planilha.getCellRangeByName(f"{col_valor_275}{linha}").getString()
+        planilha.getCellRangeByName(f"{col_valor_275}{linha}").getValue()
     )
     servidor.valor_402 = (
-        planilha.getCellRangeByName(f"{col_valor_402}{linha}").getString()
+        planilha.getCellRangeByName(f"{col_valor_402}{linha}").getValue()
     )
     servidor.valor_404 = (
-        planilha.getCellRangeByName(f"{col_valor_404}{linha}").getString()
+        planilha.getCellRangeByName(f"{col_valor_404}{linha}").getValue()
     )
 
     return servidor
@@ -229,7 +237,7 @@ def ordena_substitutos():
 # e para cada titular presente na planilha de ocorrências
 # cria uma planilha de substituição com os dados
 # do titular e de seu substituto
-def gera_planilhas_substituicoes():
+def percorre_planilha_ocorrencias():
     planilhas = get_planilhas()
     pla_oco = planilhas.getByName(PLA_OCO)
 
@@ -243,122 +251,149 @@ def gera_planilhas_substituicoes():
             linha += 1
             continue
 
-        # sanity check:
-        # verifica se o nome que consta na ocorrência está
-        # na tabela de titulares
+        ocorrencia = None
         if matricula in titulares:
             titular = titulares[matricula]
-            titular.motivo_impedimento = pla_oco.getCellRangeByName(
+            ocorrencia = Ocorrencia(titular)
+            ocorrencia.motivo_impedimento = pla_oco.getCellRangeByName(
                 f"F{linha}").getString()
-
-            titular.dias_ocorrencia = int(pla_oco.getCellRangeByName(
+            ocorrencia.dias_ocorrencia = int(pla_oco.getCellRangeByName(
                 f"I{linha}").getString())
-
-            # se o titular não tem substitutos, pula para a próxima linha
-            if len(titular.substitutos) == 0:
-                linha += 1
-                continue
-
-            # insere uma planilha no final com o nome do primeiro substituto do titular
-            if not planilhas.hasByName(titular.substitutos[0].nome_formatado):
-                # planilhas.insertNewByName(titular.nome_formatado, planilhas.Count)
-                planilhas.copyByName(
-                    PLA_MODL, titular.substitutos[0].nome_formatado, planilhas.Count)
-
-            planilha_substituicao = planilhas.getByName(
-                titular.substitutos[0].nome_formatado)
-
-            # apaga o botão Gerar Planilhas
-            colunas = planilha_substituicao.getColumns()
-            colunas.removeByIndex(11, 2)
-
-            pla_modelo = planilhas.getByName(PLA_MODL)
-
-            cels_origem = pla_modelo.getCellRangeByName("A:J")
-            cel_dest = planilha_substituicao.getCellRangeByName("A1")
-            planilha_substituicao.copyRange(
-                cel_dest.CellAddress, cels_origem.RangeAddress)
-
-            primeiro_subs = titular.substitutos[0]
-            planilha_substituicao.getCellRangeByName(
-                "E11").setString(primeiro_subs.matricula)
-            planilha_substituicao.getCellRangeByName(
-                "F11").setString(primeiro_subs.categoria)
-            planilha_substituicao.getCellRangeByName(
-                "E12").setString(primeiro_subs.nome_formatado)
-
-            if len(titular.substitutos) > 1:
-                for i in range(1, len(titular.substitutos)):
-                    subs = titular.substitutos[i]
-                    planilha_substituicao.getCellRangeByName(
-                        f"I{11+i}").setString(subs.nome_formatado)
-
-            planilha_substituicao.getCellRangeByName(
-                "E16").setString(titular.matricula)
-            planilha_substituicao.getCellRangeByName(
-                "F16").setString(titular.categoria)
-            planilha_substituicao.getCellRangeByName(
-                "E17").setString(titular.nome_formatado)
-            planilha_substituicao.getCellRangeByName(
-                "E18").setString(titular.lotacao)
-            planilha_substituicao.getCellRangeByName(
-                "E19").setString(titular.funcao_confianca)
-
-            periodo = "de " + pla_oco.getCellRangeByName(f"G{linha}").getString(
-            ) + " a " + pla_oco.getCellRangeByName(f"H{linha}").getString()
-            mes_ocorrencia = pla_oco.getCellRangeByName(f"G{linha}").getString(
+            ocorrencia.mes_ocorrencia = pla_oco.getCellRangeByName(f"G{linha}").getString(
             ).split("/")[1]
-            ano_ocorrencia = pla_oco.getCellRangeByName(f"G{linha}").getString(
+            ocorrencia.ano_ocorrencia = pla_oco.getCellRangeByName(f"G{linha}").getString(
             ).split("/")[2]
+            ocorrencia.periodo = "de " + pla_oco.getCellRangeByName(f"G{linha}").getString(
+            ) + " a " + pla_oco.getCellRangeByName(f"H{linha}").getString()
 
-            planilha_substituicao.getCellRangeByName(
-                "F22").setString(titular.motivo_impedimento)
+            gera_planilha_substituicao(ocorrencia)
 
-            planilha_substituicao.getCellRangeByName(
-                "F23").setString(periodo)
+            # # verifica se o titular tem substitutos que também são titulares,
+            # # e gera planilhas de substituição para eles, recursivamente
+            # titulares_a_verificar = [titular]
+            # while len(titulares_a_verificar) > 0:
+            #     titular_atual = titulares_a_verificar.pop()
+            #     for substituto in titular_atual.substitutos:
+            #         if substituto.matricula in titulares:
+            #             novo_titular = titulares[substituto.matricula]
+            #             nova_ocorrencia = Ocorrencia(novo_titular)
+            #             nova_ocorrencia.motivo_impedimento = "Substituição"
+            #             nova_ocorrencia.dias_ocorrencia = ocorrencia.dias_ocorrencia
+            #             nova_ocorrencia.mes_ocorrencia = ocorrencia.mes_ocorrencia
+            #             nova_ocorrencia.ano_ocorrencia = ocorrencia.ano_ocorrencia
+            #             nova_ocorrencia.periodo = ocorrencia.periodo
+            #             nova_ocorrencia.titular = novo_titular
 
-            planilha_substituicao.getCellRangeByName("G28").setFormula(
-                valores_grs[str_norm(primeiro_subs.funcao_confianca)]
-            )
+            #             titulares_a_verificar.append(novo_titular)
+            #             # apso_utils.msgbox(novo_titular.nome_formatado + " - " + nova_ocorrencia.motivo_impedimento)
+            #             arq_log.write(f"{novo_titular.nome_formatado}{nova_ocorrencia.motivo_impedimento}")
+            #             arq_log.flush()
 
-            planilha_substituicao.getCellRangeByName("G32").setFormula(
-                primeiro_subs.valor_275
-            )
-            planilha_substituicao.getCellRangeByName("G33").setFormula(
-                primeiro_subs.valor_402
-            )
-            planilha_substituicao.getCellRangeByName("G34").setFormula(
-                primeiro_subs.valor_404
-            )
-
-            planilha_substituicao.getCellRangeByName("F41").setFormula(
-                titular.dias_ocorrencia
-            )
-
-            planilha_substituicao.getCellRangeByName("C46").setString(
-                f"{get_nome_mes(mes_ocorrencia)}({titular.dias_ocorrencia})"
-            )
-
-            planilha_substituicao.getCellRangeByName("E46").setFormula(
-                f"=(G38/30)*{titular.dias_ocorrencia}"
-            )
-
-            if primeiro_subs.deduzir_insalubridade:
-                planilha_substituicao.getCellRangeByName("G46").setFormula(
-                    "SIM ( X ) CÓDIGO 031- "
-                )
-
-                # mostra o número de dias a serem deduzidos se o SIM for marcado
-                planilha_substituicao.getCellRangeByName("I46").setFormula(
-                    monthrange(int(ano_ocorrencia), int(mes_ocorrencia))[
-                        1] - titular.dias_ocorrencia
-                )
-            else:
-                planilha_substituicao.getCellRangeByName("G48").setFormula(
-                    "NÃO ( X )."
-                )
-
+            #             gera_planilha_substituicao(nova_ocorrencia)
         linha += 1
+
+
+def gera_planilha_substituicao(ocorrencia):
+    planilhas = get_planilhas()
+
+    titular = ocorrencia.titular
+
+    # se o titular não tem substitutos, não é necessário gerar planilhas
+    if len(titular.substitutos) == 0:
+        return
+
+    # insere uma planilha no final com o nome do primeiro substituto do titular
+    if not planilhas.hasByName(titular.substitutos[0].nome_formatado):
+        # planilhas.insertNewByName(titular.nome_formatado, planilhas.Count)
+        planilhas.copyByName(
+            PLA_MODL, titular.substitutos[0].nome_formatado, planilhas.Count)
+
+    planilha_substituicao = planilhas.getByName(
+        titular.substitutos[0].nome_formatado)
+
+    # apaga o botão Gerar Planilhas
+    colunas = planilha_substituicao.getColumns()
+    colunas.removeByIndex(11, 2)
+
+    pla_modelo = planilhas.getByName(PLA_MODL)
+
+    cels_origem = pla_modelo.getCellRangeByName("A:J")
+    cel_dest = planilha_substituicao.getCellRangeByName("A1")
+    planilha_substituicao.copyRange(
+        cel_dest.CellAddress, cels_origem.RangeAddress)
+
+    primeiro_subs = titular.substitutos[0]
+    planilha_substituicao.getCellRangeByName(
+        "E11").setString(primeiro_subs.matricula)
+    planilha_substituicao.getCellRangeByName(
+        "F11").setString(primeiro_subs.categoria)
+    planilha_substituicao.getCellRangeByName(
+        "E12").setString(primeiro_subs.nome_formatado)
+
+    if len(titular.substitutos) > 1:
+        for i in range(1, len(titular.substitutos)):
+            subs = titular.substitutos[i]
+            planilha_substituicao.getCellRangeByName(
+                f"I{11+i}").setString(subs.nome_formatado)
+
+    planilha_substituicao.getCellRangeByName(
+        "E16").setString(titular.matricula)
+    planilha_substituicao.getCellRangeByName(
+        "F16").setString(titular.categoria)
+    planilha_substituicao.getCellRangeByName(
+        "E17").setString(titular.nome_formatado)
+    planilha_substituicao.getCellRangeByName(
+        "E18").setString(titular.lotacao)
+    planilha_substituicao.getCellRangeByName(
+        "E19").setString(titular.funcao_confianca)
+
+    planilha_substituicao.getCellRangeByName(
+        "F22").setString(ocorrencia.motivo_impedimento)
+
+    planilha_substituicao.getCellRangeByName(
+        "F23").setString(ocorrencia.periodo)
+
+    planilha_substituicao.getCellRangeByName("G28").setFormula(
+        "=" + str(valores_grs[str_norm(primeiro_subs.funcao_confianca)])
+    )
+
+    planilha_substituicao.getCellRangeByName("G32").setValue(
+        primeiro_subs.valor_275
+    )
+    planilha_substituicao.getCellRangeByName("G33").setValue(
+        primeiro_subs.valor_402
+    )
+    planilha_substituicao.getCellRangeByName("G34").setValue(
+        primeiro_subs.valor_404
+    )
+
+    planilha_substituicao.getCellRangeByName("F41").setValue(
+        ocorrencia.dias_ocorrencia
+    )
+
+    planilha_substituicao.getCellRangeByName("C46").setString(
+        get_nome_mes(ocorrencia.mes_ocorrencia) +
+        "(" + str(ocorrencia.dias_ocorrencia) + ")"
+    )
+
+    planilha_substituicao.getCellRangeByName("E46").setFormula(
+        f"=(G38/30)*{ocorrencia.dias_ocorrencia}"
+    )
+
+    if primeiro_subs.deduzir_insalubridade:
+        planilha_substituicao.getCellRangeByName("G46").setString(
+            "SIM ( X ) CÓDIGO 031- "
+        )
+
+        # mostra o número de dias a serem deduzidos se o SIM for marcado
+        planilha_substituicao.getCellRangeByName("I46").setString(
+            str(monthrange(int(ocorrencia.ano_ocorrencia), int(ocorrencia.mes_ocorrencia))[
+                1] - ocorrencia.dias_ocorrencia)
+        )
+    else:
+        planilha_substituicao.getCellRangeByName("G48").setString(
+            "NÃO ( X )."
+        )
 
 
 def main(arg):
@@ -366,4 +401,4 @@ def main(arg):
     preenche_substitutos()
     ordena_substitutos()
     preenche_valores_grs()
-    gera_planilhas_substituicoes()
+    percorre_planilha_ocorrencias()
